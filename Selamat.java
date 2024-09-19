15585fca84fee316632bb1a336f907598e30daf8
Sannfor <ahmadihasan22111999@gmail.com>
2024-09-20 05:09:01 +0800

    Add files via upload

---
+++ Selamat.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package selamat;
+
+    public class Selamat {
+        public static void main(String[] args) {
+            String x =  "Selamat Datang", y = "Silahkan ke tempat duduk anda";
+            System.out.println(x+ "\n" + y);
+    
+        }
+}
